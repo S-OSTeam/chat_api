da6071a1f7fba6c65a318572c048a3a3a9b45396
조민혁 <71816821+minhyuk2@users.noreply.github.com>
2025-01-06 17:17:49 +0900

    faet : entity들 생성

---
+++ src/main/java/sos/chat_api/domain/chat/entity/ChatRoom.java
@@ -0,0 +1,22 @@
+package sos.chat_api.domain.chat.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class ChatRoom {
+
+    @Id
+    @GeneratedValue(strategy =  GenerationType.IDENTITY)
+    private Long chat_id;
+
+    private String chat_name;
+
+    private Long chat_people;
+
+    //이 부분은 키로 연결할지 아니면 그냥 여기에 값을 넣어줄지
+    private Long user_id;
+
+}
---
+++ src/main/java/sos/chat_api/domain/chat/entity/Message.java
@@ -0,0 +1,16 @@
+package sos.chat_api.domain.chat.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Message {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long message_id;
+
+
+}
