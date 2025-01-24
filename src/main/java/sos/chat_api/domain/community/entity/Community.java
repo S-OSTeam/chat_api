5ad7b082e2a8c047c245a4cece868bbc78db1108
조민혁 <71816821+minhyuk2@users.noreply.github.com>
2025-01-24 22:23:47 +0900

    feat : community entity 생성

---
+++ src/main/java/sos/chat_api/domain/community/entity/Community.java
@@ -0,0 +1,23 @@
+package sos.chat_api.domain.community.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import sos.chat_api.domain.category.entity.Category;
+
+@Entity
+@Getter
+@Setter
+public class Community {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long community_id;
+
+    @ManyToOne
+    @JoinColumn(name="category_id")
+    private Category category;
+
+    private String community_name;
+
+}
