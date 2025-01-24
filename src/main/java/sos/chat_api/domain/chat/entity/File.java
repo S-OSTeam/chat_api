ce20b43c6b0e75663d855d0c44fec04d5593d2eb
조민혁 <71816821+minhyuk2@users.noreply.github.com>
2025-01-24 18:36:51 +0900

    fix : file entity 생성

---
+++ src/main/java/sos/chat_api/domain/chat/entity/File.java
@@ -0,0 +1,25 @@
+package sos.chat_api.domain.chat.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.UUID;
+@Entity
+@Getter
+@Setter
+public class File {
+
+    @Id
+    @GeneratedValue(strategy =  GenerationType.IDENTITY)
+    private Long file_id;
+
+    @OneToOne
+    @JoinColumn(name = "message_id")
+    private Message message;
+
+    private String file_url;
+
+    //만들어서 설정해서 넣어주면 된다.
+    private UUID file_name;
+}
