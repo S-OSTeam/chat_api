fe629da27ae4e0d5045209f904af358d01ac6ed3
조민혁 <71816821+minhyuk2@users.noreply.github.com>
2025-02-07 16:40:56 +0900

    fix : 이미지 버킷에 올리는 부분 추가

--- src/main/java/sos/chat_api/domain/board/service/BoardService.java
+++ src/main/java/sos/chat_api/domain/board/service/BoardService.java
@@ -16,6 +16,7 @@ import sos.chat_api.domain.category.entity.Category;
 import sos.chat_api.domain.category.repository.CategoryRepository;
 import sos.chat_api.domain.community.entity.Community;
 import sos.chat_api.domain.community.repository.CommunityRepository;
+import sos.chat_api.domain.s3code.AwsFileService;
 import sos.chat_api.domain.user.entity.User;
 import sos.chat_api.domain.user.repository.UserRepository;
 import sos.chat_api.global.exception.BaseResponse;
@@ -31,7 +32,7 @@ public class BoardService {
     private final CommunityRepository communityRepository;
     private final CategoryRepository categoryRepository;
     private final UserRepository userRepository;
-
+    private final AwsFileService awsFileService;
     private final BaseResponseServiceImpl baseResponseService;
 
     //글 작성
@@ -46,9 +47,15 @@ public class BoardService {
         User userEntity = userRepository.findById(boardDTO.getUserId())
                 .orElseThrow(()->new RuntimeException("User not found"));
 
-        Board board = new Board(community,category,userEntity,boardDTO.getBoardTitle(),boardDTO.getBoardDetail());
-
-        //이미지 설정하는 부분 필요
+        Board board;
+        if(!boardDTO.getBoardImage().isEmpty()) {
+                String imageUrl = awsFileService.upload(boardDTO.getBoardImage());
+                board = new Board(community,category,userEntity,boardDTO.getBoardTitle(),boardDTO.getBoardDetail());
+                board.setImage_url(imageUrl);
+                board.setImage_name(boardDTO.getBoardImage().getName());
+            }else{
+            board = new Board(community,category,userEntity,boardDTO.getBoardTitle(),boardDTO.getBoardDetail());
+        }
         boardRepository.save(board);
         return board;
     }
@@ -59,7 +66,8 @@ public class BoardService {
         return boardRepository.findById(boardId).orElseThrow(()->new RuntimeException("Board not found"));
     }
 
-    //이미지 올리기
-    //S3를 사용할건지 어떻게 할지
+    //삭제
+
+    //변경
 
 }
