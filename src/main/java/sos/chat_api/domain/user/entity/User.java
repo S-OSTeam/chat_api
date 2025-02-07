07b6e7ba9b662dead7688375c66df8724d2ba69f
조민혁 <71816821+minhyuk2@users.noreply.github.com>
2025-02-07 15:34:34 +0900

    fix : user table 설정

--- src/main/java/sos/chat_api/domain/user/entity/User.java
+++ src/main/java/sos/chat_api/domain/user/entity/User.java
@@ -1,9 +1,6 @@
 package sos.chat_api.domain.user.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -12,6 +9,7 @@ import lombok.Setter;
 @NoArgsConstructor
 @Getter
 @Setter
+@Table(name = "users")
 public class User {
 
     @Id
